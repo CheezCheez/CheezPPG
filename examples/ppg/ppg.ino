d0d02936d467043eacd02618d2ea7c5543ba6804
cheez <xuweican98@gmail.com>
2025-04-29 13:28:48 +0800

    修改示例代码说明

--- examples/ppg/ppg.ino
+++ examples/ppg/ppg.ino
@@ -1,3 +1,31 @@
+/**
+******************************************************************************
+* @file   : ppg.ino
+* @brief  ：需安装Arduino库 CheezPPG
+* @brief  : 输出：串口,波特率 115200
+            采样率：125Hz 
+            引脚连接：
+                  单片机  |   传感器
+                  D2      |   S2(黄色线)
+                  A0      |   S1(白色线)
+                  5V      |   V(红色线)
+                  GND     |   G(黑色线)
+
+            
+            串口输出内容(ASCII码)：
+                  原始数据,平滑滤波数据,带通滤波数据,心跳检测数据,心率数据,HRV(SDNN)数据
+******************************************************************************
+* @attention
+*
+* Copyright (c) 2024 Cheez.
+* All rights reserved.
+*
+* This software is licensed under terms that can be found in the LICENSE file
+* in the root directory of this software component.
+* If no LICENSE file comes with this software, it is provided AS-IS.
+*
+******************************************************************************
+*/  
 #include "CheezPPG.h"
  
 #define INPUT_PIN A0    // PPG引脚
@@ -17,13 +45,12 @@ void loop()
   {  
     ppg.ppgProcess();
     Serial.println(
-      String((int)ppg.getRawPPG()) + "," + 
-      String((int)ppg.getAvgPPG()) + "," + 
-      String((int)ppg.getFilterPPG()) + "," +  
-      String((int)ppg.getPpgPeak()) + "," + 
-      String((int)ppg.getPpgHr())+ "," + 
-      String((int)ppg.getPpgHrv()) 
+      String((int)ppg.getRawPPG())    + "," +  // 原始数据
+      String((int)ppg.getAvgPPG())    + "," +  // 平滑滤波数据
+      String((int)ppg.getFilterPPG()) + "," +  // 带通滤波数据
+      String((int)ppg.getPpgPeak())   + "," +  // 心跳检测数据
+      String((int)ppg.getPpgHr())     + "," +  // 心率数据
+      String((int)ppg.getPpgHrv())             // HRV(SDNN)数据
     );
   }   
 }
-
