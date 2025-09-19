73017c67d0d2f53b3900f12ab260b80043744274
cheez <xuweican98@gmail.com>
2025-09-19 11:48:52 +0800

    v1.1.3

--- src/CheezPPG.h
+++ src/CheezPPG.h
@@ -1,23 +1,23 @@
 /**
  * @file CheezPPG.h
  * @author Vecang @Cheez
- * @brief 
- * @version 1.1.1 
+ * @brief
+ * @version 1.1.3
  * @date 2025-09-19
- * 
+ *
  * @copyright Copyright (c) 2025
-******************************************************************************
-* @attention
-*
-* Copyright (c) 2024 Cheez.
-* All rights reserved.
-*
-* This software is licensed under terms that can be found in the LICENSE file
-* in the root directory of this software component.
-* If no LICENSE file comes with this software, it is provided AS-IS.
-*
-******************************************************************************
-*/
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 Cheez.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 
 #ifndef _CHEEZPPG_H_
 #define _CHEEZPPG_H_
@@ -26,9 +26,12 @@
 
 class CheezPPG {
  public:
-  CheezPPG(int inputPin, int sampleRate);  // 构造函数，初始化输入引脚和采样率
-  bool checkSampleInterval(void);  // 检查采样间隔是否到达
+  CheezPPG(int inputPin, int sampleRate);
+  CheezPPG(int inputPin, int sampleRate, unsigned char hr_min,
+           unsigned char hr_max);
+  ~CheezPPG() {};
 
+  bool checkSampleInterval(void);  // 检查采样间隔是否到达
   void ppgProcess(void);
   void detectWearStatus();
   void setWearThreshold(int Threshold);
@@ -46,31 +49,35 @@ class CheezPPG {
   int _rawPPG = 0;
   int _avgPPG = 0;
   int _filteredPPG = 0;
-  unsigned char ispeak = 0;        // 当前点是否为峰/谷的标志
-  unsigned char _ispeakPeak = 0;   // 当前点是否为峰的标志
-  unsigned char _ispeakOnset = 0;  // 当前点是否为谷的标志
+  unsigned char ispeak = 0;
+  unsigned char _ispeakPeak = 0;
+  unsigned char _ispeakOnset = 0;
   unsigned char heart_rate = 0;
   float sdnn = 0;
 
   uint32_t _inputPin;
-  uint32_t _sampleRate = 0;  // 采样间隔计数器
+  uint32_t _sampleRate = 0;
 
-  unsigned long _peak_window_HP[21];  // 用于存储21个点的峰值检测窗口
-  unsigned char _HR_buffer[12];       // 存储最近12个心率值的数组
-  unsigned char _HRV_buffer[12];      // 存储最近12个HRV(SDNN)值的数组
-  float _RR_buffer[12];               // 存储最近12个RR间期的数组
-  float _avg_PPG_buffer[5];           // 平滑滤波缓冲区
+  static const int PEAK_WINDOW_SIZE = 21;  // 峰值检测窗口大小(奇数)
+  static const int PEAK_WINDOW_HALF_SIZE = (PEAK_WINDOW_SIZE - 1) / 2;
+  static const int _WINDOW_SIZE = 12;  // 心率/HRV值的数组大小
+
+  unsigned long _peak_window_HP[PEAK_WINDOW_SIZE];  // 峰值检测窗口
+  unsigned char _HR_buffer[_WINDOW_SIZE];           // 心率值
+  unsigned char _HRV_buffer[_WINDOW_SIZE];          // HRV(SDNN)
+  float _RR_buffer[_WINDOW_SIZE];                   // RR间期
+
   unsigned long _moving_window_HP = 0;     // 移动平均累加器
   unsigned char _moving_window_count = 0;  // 移动平均计数器
   unsigned char _moving_window_size = 0;   // 移动平均窗口大小
-  unsigned char _smallest = 0;  // 最小可能的心跳间隔（对应最高心率）
-  unsigned char _found_peak = 0;        // 已找到的峰/谷计数
-  unsigned char _total_found_peak = 0;  // 总共找到的峰/谷数
-  unsigned int _last_peak = 0;          // 距离上一个峰值的采样点数
-  unsigned int _last_onset = 0;         // 距离上一个谷值的采样点数
+  unsigned char _smallest = 0;             // 最小可能的心跳间隔
+  unsigned char _found_peak = 0;           // 已找到的峰/谷计数
+  unsigned char _total_found_peak = 0;     // 总共找到的峰/谷数
+  unsigned int _last_peak = 0;   // 距离上一个峰值的采样点数
+  unsigned int _last_onset = 0;  // 距离上一个谷值的采样点数
 
-  unsigned long _last_onsetValueLED1;  // LED1信号的上一个谷值
-  unsigned long _last_peakValueLED1;   // LED1信号的上一个峰值
+  unsigned long _last_onsetValueLED1;
+  unsigned long _last_peakValueLED1;
 
   unsigned char _hr_temp = 0;
   unsigned char _hr_max = 220;  // 最大心率
